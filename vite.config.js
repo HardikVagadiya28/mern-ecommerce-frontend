f065da3286ec182e14dfcb4f11caa8fd2434b02c
HardikVagadiya28 <hardikvagadiya2003@gmail.com>
2025-09-19 16:20:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0', // આ લાઇન ઉમેરો
+    port: process.env.PORT || 4173 // આ પણ ઉમેરો
+  }
 })
